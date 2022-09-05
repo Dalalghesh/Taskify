d547a9b40165751a576d6bf601ab6964c4945dd3
Dalalghesh <Dalal.ghesh@gmail.com>
2022-09-05 15:35:01 +0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Dalal Gheshiyan on 05/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TaskifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskifyApp.swift
+//  Shared
+//
+//  Created by Dalal Gheshiyan on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
