6e389dc1e772bfddc1df99c3cdb18aede9d6b458
Dalalghesh <Dalal.ghesh@gmail.com>
2022-09-06 16:01:33 +0300

    testing database

--- Shared/TaskifyApp.swift
+++ Shared/TaskifyApp.swift
@@ -7,12 +7,30 @@
 
 import SwiftUI
 import FirebaseCore
+import Firebase
+import FirebaseFirestore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
-
+      
+      let db = Firestore.firestore()
+      
+      // Add a new document with a generated ID
+      var ref: DocumentReference? = nil
+      ref = db.collection("users").addDocument(data: [
+          "first": "Ada",
+          "last": "Lovelace",
+          "born": 1815
+      ]) { err in
+          if let err = err {
+              print("Error adding document: \(err)")
+          } else {
+              print("Document added with ID: \(ref!.documentID)")
+          }
+      }
+      
     return true
   }
 }
