f85d3e07b812befe8657ae472c08da9afc9451e4
RaghadSD <86389094+RaghadSD@users.noreply.github.com>
2022-09-07 18:56:46 +0300

    Set up files

---
+++ taskify/lib/Screens/AddList.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AddList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Add List')));
+  }
+}
---
+++ taskify/lib/Screens/AddTask.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AddTask extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Add Task')));
+  }
+}
