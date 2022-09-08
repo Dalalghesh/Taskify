a981556606941795a4fe7b272c0935afc8c5f111
RaghadSD <86389094+RaghadSD@users.noreply.github.com>
2022-09-08 04:46:21 +0300

    Add List

--- taskify/lib/Screens/AddList.dart
+++ taskify/lib/Screens/AddList.dart
@@ -1,8 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:taskify/Screens/AddTask.dart';
+import 'package:taskify/Screens/InviteFriend.dart';
+import 'package:taskify/util.dart';
 
 class AddList extends StatelessWidget {
+  //const SendInstructionsView({Key? key}) : super(key: key);
+  bool buttonenabled = false;
+  String? selectedValue;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Add List')));
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 50,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Text(
+              'Create List',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'List Name:',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Container(
+              height: 50,
+              child: TextFormField(
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Categorey:',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            DropdownButton<String>(
+              //style: Theme.of(context).inputDecorationTheme.border,
+              focusColor: Color.fromARGB(255, 69, 31, 156),
+              borderRadius: BorderRadius.circular(25),
+              isExpanded: true,
+              hint: Text('Select any category', style: TextStyle(fontSize: 15)),
+              items: <String>['Home', 'University', 'Work', 'Grocery']
+                  .map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (_) {},
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CheckboxListTile(
+              title: Text("Do you want it to be shared?"),
+              value: buttonenabled,
+              onChanged: (_) {},
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: ElevatedButton(
+                  onPressed: () {
+                    //navigate to check email view
+                    Util.routeToWidget(context, InviteFriend());
+                  },
+                  child: Text(
+                    'Create',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- taskify/lib/main.dart
+++ taskify/lib/main.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:taskify/Screens/AddList.dart';
+//import 'package:taskify/send_instructions/send_instructions_view.dart';
+// #7b39ed - primary color
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final MaterialColor primarySwatch = MaterialColor(0xff7b39ed, <int, Color>{
+    50: Color(0xff7b39ed),
+    100: Color(0xff7b39ed),
+    200: Color(0xff7b39ed),
+    300: Color(0xff7b39ed),
+    400: Color(0xff7b39ed),
+    500: Color(0xff7b39ed),
+    600: Color(0xff7b39ed),
+    700: Color(0xff7b39ed),
+    800: Color(0xff7b39ed),
+    900: Color(0xff7b39ed),
+  });
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Taskify ', home: homePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Taskify',
+      theme: ThemeData(
+        iconTheme: IconThemeData(color: Color(0xff7b39ed)),
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            )),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              minimumSize: Size(double.infinity, 50),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              )),
+        ),
+        textTheme: TextTheme(
+            headline4:
+                TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            subtitle1: TextStyle(
+              color: Colors.grey.shade600,
+            )),
+        appBarTheme: AppBarTheme(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            iconTheme: IconThemeData(color: Colors.black)),
+        primaryColor: Color(0xff7b39ed),
+        primarySwatch: primarySwatch,
+      ),
+      home: AddList(),
+    );
   }
 }
