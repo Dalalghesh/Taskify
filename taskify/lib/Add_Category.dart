281b9086f7ebb2d3c5be7e5ddf6bdf546c70f0f6
Dalalghesh <Dalal.ghesh@gmail.com>
2022-09-13 20:23:05 +0300

    Update Add_Category.dart

--- taskify/lib/Add_Category.dart
+++ taskify/lib/Add_Category.dart
@@ -1,11 +1,10 @@
 
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:taskify/util.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:cool_alert/cool_alert.dart';
 
 
