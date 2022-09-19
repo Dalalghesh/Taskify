e72545a482dc16100c5792bf7761235a069a44ad
RaghadSD <86389094+RaghadSD@users.noreply.github.com>
2022-09-19 22:15:56 +0300

    update profile screen

--- taskify/lib/screens/ProfileScreen/profileScreen.dart
+++ taskify/lib/screens/ProfileScreen/profileScreen.dart
@@ -6,56 +6,32 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-  Widget textfield({@required hintText}) {
-    return Material(
-      // elevation: 4,
-      // shadowColor: Colors.grey,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      // child: TextField(
-      //   decoration: InputDecoration(
-      //       hintText: hintText,
-      //       hintStyle: TextStyle(
-      //         letterSpacing: 2,
-      //         color: Colors.black54,
-      //         //  fontWeight: FontWeight.bold,
-      //       ),
-      //       fillColor: Colors.white30,
-      //       filled: true,
-      //       contentPadding: EdgeInsets.symmetric(
-      //         vertical: 10,
-      //         horizontal: 10,
-      //       ),
-      //       border: OutlineInputBorder(
-      //           borderRadius: BorderRadius.circular(10.0),
-      //           borderSide: BorderSide.none)),
-      // ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 0.0,
         backgroundColor: Color.fromRGBO(123, 57, 237, 1),
-        leading: IconButton(
-          padding: EdgeInsets.only(
-            left: 380,
+        elevation: 0.0,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            padding: EdgeInsets.only(
+              right: 10,
+            ),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => const LoginScreen()),
+              );
+            },
+            icon: Icon(
+              Icons.logout_outlined,
+              color: Color.fromRGBO(255, 255, 255, 1),
+              size: 35,
+            ),
           ),
-          icon: Icon(
-            Icons.logout_outlined,
-            color: Color.fromRGBO(255, 255, 255, 1),
-            size: 30,
-          ),
-          onPressed: () async {
-            await FirebaseAuth.instance.signOut();
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => const LoginScreen()),
-            );
-          },
-        ),
+        ],
       ),
       body: Stack(
         alignment: Alignment.center,
@@ -68,7 +44,6 @@ class HomeScreen extends StatelessWidget {
                 width: double.infinity,
                 margin: EdgeInsets.symmetric(horizontal: 10),
                 child: Column(
-                  //  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     SizedBox(
                       height: 19,
@@ -77,18 +52,14 @@ class HomeScreen extends StatelessWidget {
                       '${FirebaseAuth.instance.currentUser!.displayName}',
                       style: TextStyle(
                         fontSize: 25,
-                        //letterSpacing: 1.5,
                         color: Color.fromARGB(255, 0, 0, 0),
-                        //   fontWeight: FontWeight.w600,
                       ),
                     ),
                     Text(
                       '${FirebaseAuth.instance.currentUser!.email}',
                       style: TextStyle(
                         fontSize: 18,
-                        //letterSpacing: 1.5,
                         color: Color.fromARGB(255, 0, 0, 0),
-                        //   fontWeight: FontWeight.w600,
                       ),
                     ),
                     SizedBox(
@@ -131,7 +102,6 @@ class HomeScreen extends StatelessWidget {
                   "My profile",
                   style: TextStyle(
                     fontSize: 35,
-                    //letterSpacing: 1.5,
                     color: Colors.white,
                     fontWeight: FontWeight.w600,
                   ),
@@ -153,57 +123,10 @@ class HomeScreen extends StatelessWidget {
               ),
             ],
           ),
-          // Padding(
-          //   padding: EdgeInsets.only(bottom: 300, left: 164),
-          //   child: CircleAvatar(
-          //     backgroundColor: Color(0xff7b39ed),
-          //     child: IconButton(
-          //       icon: Icon(
-          //         Icons.edit,
-          //         color: Colors.white,
-          //       ),
-          //       onPressed: () {},
-          //     ),
-          //   ),
-          // )
         ],
       ),
     );
   }
-  //@override
-  //Widget build(BuildContext context) {
-  // return Scaffold(
-  //   body: SafeArea(
-  //     child: Padding(
-  //       padding: const EdgeInsets.all(16.0),
-  //       child: Column(
-  //         mainAxisSize: MainAxisSize.max,
-  //         crossAxisAlignment: CrossAxisAlignment.center,
-  //         mainAxisAlignment: MainAxisAlignment.center,
-  //         children: [
-  //           Text('UID: ${FirebaseAuth.instance.currentUser!.uid}'),
-  //           const SizedBox(height: 12),
-  //           Text('Email: ${FirebaseAuth.instance.currentUser!.email}'),
-  //           const SizedBox(height: 12),
-  //           Text('Name: ${FirebaseAuth.instance.currentUser!.displayName}'),
-  //           const SizedBox(height: 12),
-  //           Center(
-  //             child: PrimaryButton(
-  //               onTap: () async {
-  //                 await FirebaseAuth.instance.signOut();
-  //                 Navigator.of(context).pushReplacement(
-  //                   MaterialPageRoute(builder: (context) => const LoginScreen()),
-  //                 );
-  //               },
-  //               text: "Logout",
-  //             ),
-  //           )
-  //         ],
-  //       ),
-  //     ),
-  //   ),
-  // );
-  //}
 }
 
 class HeaderCurvedContainer extends CustomPainter {
