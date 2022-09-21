b852f381407bac8168db15390b4ca321639cd34e
Dalalghesh <Dalal.ghesh@gmail.com>
2022-09-21 17:44:45 +0300

    update

---
+++ taskify/ios/Runner/AppDelegate 2.swift
@@ -0,0 +1,24 @@
+import UIKit
+import Flutter
+import FirebaseMessaging
+import Firebase
+
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+    override func application (_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data){
+    Messaging.messaging().apnsToken = deviceToken
+    print("Token: \(deviceToken)")
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+    
+  }
+}
