a7472a411b5559a66aa225c73d3ce55983641445
RaghadSD <86389094+RaghadSD@users.noreply.github.com>
2022-09-25 00:11:35 +0300

    Style and updates accept reject

---
+++ taskify/lib/invitation/widget/send_invitation_form.dart
@@ -0,0 +1,177 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:provider/provider.dart';
+import 'package:taskify/homePage.dart';
+
+import 'package:taskify/screens/AddList.dart';
+
+import '../../util.dart';
+import '../../utils/validators.dart';
+import '../provider/invitation.dart';
+import '../screens/received_invitations.dart';
+import 'package:cool_alert/cool_alert.dart';
+
+class SendInvitationForm extends StatefulWidget {
+  const SendInvitationForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SendInvitationForm> createState() => _SendInvitationFormState();
+}
+
+class _SendInvitationFormState extends State<SendInvitationForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _typeAheadController = TextEditingController();
+  String query = '';
+  String? email;
+  Future<void> sendInviation() async {
+    try {
+      final validate = _formKey.currentState?.validate();
+      if (validate ?? false) {
+        _formKey.currentState?.save();
+        print(email.toString());
+        await context.read<InvitaitonProvider>().sendInvitation(email!);
+        CoolAlert.show(
+          context: context,
+          type: CoolAlertType.success,
+          text: "Invitation sent successfully",
+          confirmBtnColor: const Color(0xff7b39ed),
+          onConfirmBtnTap: () => route(),
+          // onConfirmBtnTap: () => route(isChecked),
+        );
+        // showPlatformDialogue(
+        //     context: context, title: "Invitation sent successfully");
+        _formKey.currentState?.reset();
+      }
+    } catch (e) {
+      _formKey.currentState?.reset();
+      //showPlatformDialogue(context: context, title: e.toString());
+      CoolAlert.show(
+        context: context,
+        type: CoolAlertType.error,
+        text: "A user with that email does not exists",
+        confirmBtnColor: const Color(0xff7b39ed),
+        // onConfirmBtnTap: () => route(isChecked),
+      );
+    }
+    _typeAheadController.clear();
+  }
+  // List<String> emails = [];
+
+  @override
+  Widget build(BuildContext context) {
+    InvitaitonProvider provider = Provider.of<InvitaitonProvider>(context);
+    String name = "";
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 70,
+            child: TypeAheadFormField(
+              textFieldConfiguration: TextFieldConfiguration(
+                  controller: this._typeAheadController,
+                  decoration: InputDecoration(labelText: 'Email'),
+                  onChanged: (val) {
+                    query = val;
+                    provider.filterEmail(query);
+                  }),
+              suggestionsCallback: (pattern) {
+                return provider.filteredEmails;
+              },
+              itemBuilder: (context, suggestion) {
+                return ListTile(
+                  title: Text(suggestion.toString()),
+                );
+              },
+              transitionBuilder: (context, suggestionsBox, controller) {
+                return suggestionsBox;
+              },
+              onSuggestionSelected: (suggestion) {
+                this._typeAheadController.text = suggestion.toString();
+                email = suggestion.toString();
+              },
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter email';
+                }
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 0,
+          ),
+          ElevatedButton(
+            onPressed: () async => await sendInviation(),
+            child: const Text(
+              'Invite',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          // const SizedBox(
+          //   height: 16,
+          // ),
+          // GestureDetector(
+          //   onTap: () {
+          //     Navigator.of(context).pushNamed(RecievedInvitations.routeName);
+          //   },
+          //   child: const Text("Check your invitations"),
+          // ),
+          // const SizedBox(
+          //   height: 5,
+          // ),
+          Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.grey.shade600,
+                    textStyle: const TextStyle(fontSize: 18),
+                  ),
+                  onPressed: () {
+                    Util.routeToWidget(context, NavBar(tabs: 0));
+                  },
+                  child: const Text('Later'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(
+      {VoidCallback? onTap, required String text, Color? color}) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: MaterialButton(
+        color: color,
+        minWidth: double.infinity,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        onPressed: onTap,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void route() {
+    //route it to home page
+    Util.routeToWidget(context, NavBar(tabs: 0));
+  }
+}
--- taskify/lib/main.dart
+++ taskify/lib/main.dart
@@ -1,76 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:taskify/Screens/AddList.dart';
+import 'package:provider/provider.dart';
+import 'package:taskify/appstate.dart';
+import 'package:taskify/invitation/provider/invitation.dart';
+import 'package:taskify/screens/send_instructions_view.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:taskify/screens/AddTask.dart';
 import 'firebase_options.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_cupertino_datetime_picker/flutter_cupertino_datetime_picker.dart';
-import 'package:get/get.dart';
+
+import 'package:taskify/onboarding/onboarding_screen.dart';
+// #7b39ed - primary color
 
 void main() async {
-  //Initializing Database when starting the application.
-
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const int _deepPurplePrimaryValue = 0xFF673AB7;
+
   final MaterialColor primarySwatch = MaterialColor(0xff7b39ed, <int, Color>{
-    50: Color(0xff7b39ed),
-    100: Color(0xff7b39ed),
-    200: Color(0xff7b39ed),
-    300: Color(0xff7b39ed),
-    400: Color(0xff7b39ed),
-    500: Color(0xff7b39ed),
-    600: Color(0xff7b39ed),
-    700: Color(0xff7b39ed),
-    800: Color(0xff7b39ed),
-    900: Color(0xff7b39ed),
+    50: Color(0xFFEDE7F6),
+    100: Color(0xFFD1C4E9),
+    200: Color(0xFFB39DDB),
+    300: Color(0xFF9575CD),
+    400: Color(0xFF7E57C2),
+    500: Color(_deepPurplePrimaryValue),
+    600: Color(0xFF5E35B1),
+    700: Color(0xFF512DA8),
+    800: Color(0xFF4527A0),
+    900: Color(0xFF311B92),
   });
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Taskify',
-      theme: ThemeData(
-        iconTheme: IconThemeData(color: Color(0xff7b39ed)),
-        inputDecorationTheme: InputDecorationTheme(
-            contentPadding: EdgeInsets.symmetric(
-              vertical: 12,
-              horizontal: 10,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => InvitaitonProvider())
+      ],
+      child: ChangeNotifierProvider(
+        create: (context) => AppState(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            iconTheme: IconThemeData(color: Color(0xff7b39ed)),
+            inputDecorationTheme: InputDecorationTheme(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.grey.shade400),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  )),
             ),
-            //contentPadding: EdgeInsets.all(10),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide(color: Colors.grey.shade400),
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-            )),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              minimumSize: Size(double.infinity, 50),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              )),
+            textTheme: TextTheme(
+                headline4:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                subtitle1: TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                )),
+            appBarTheme: AppBarTheme(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                iconTheme: IconThemeData(color: Colors.black)),
+            primaryColor: Color(0xff7b39ed),
+            primarySwatch: primarySwatch,
+          ),
+          home: OnboardingScreen(),
         ),
-        textTheme: TextTheme(
-            headline4:
-                TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-            subtitle1: TextStyle(
-              color: Colors.black,
-            )),
-        appBarTheme: AppBarTheme(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            iconTheme: IconThemeData(color: Colors.black)),
-        primaryColor: Color(0xff7b39ed),
-        primarySwatch: primarySwatch,
       ),
-      home: AddList(),
     );
   }
 }
