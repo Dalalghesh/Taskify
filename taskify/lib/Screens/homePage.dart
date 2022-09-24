4f492d387adf282cdb7470bb3a33ce07ac2431ba
RaghadSD <86389094+RaghadSD@users.noreply.github.com>
2022-09-25 01:46:08 +0300

    Delete duplicates

--- taskify/lib/Screens/homePage.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class homePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Home Page')));
-  }
-}
