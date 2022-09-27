2aa8e765f722f45140a1eabbe407bdf1fa06f42b
Dalalghesh <Dalal.ghesh@gmail.com>
2022-09-27 05:15:38 +0300

    Notifications Sent successfully - for the current user

--- taskify/lib/invitation/widget/send_invitation_form.dart
+++ taskify/lib/invitation/widget/send_invitation_form.dart
@@ -7,6 +7,7 @@ import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:provider/provider.dart';
 import 'package:taskify/homePage.dart';
 import 'package:taskify/screens/AddList.dart';
+import 'package:taskify/service/local_push_notification.dart';
 import '../../util.dart';
 import '../../utils/validators.dart';
 import '../provider/invitation.dart';
@@ -32,10 +33,61 @@ class _SendInvitationFormState extends State<SendInvitationForm> {
   final TextEditingController _typeAheadController = TextEditingController();
   String query ='';
   String? email;
+
+   void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseMessaging.instance.getInitialMessage();
+    FirebaseMessaging.onMessage.listen((event) {
+      LocalNotificationService.display(event);
+    });
+    storenotificationToken();
+
+    FirebaseMessaging.instance.subscribeToTopic('subscription');
+   sendNotification('New Invitation', 'd6XLoEIsSqSkQpDK6FQsC8:APA91bHHHi5qZNEz8hsmpweko4MhuEYqKndjSRQsZs06W91T5Zy9MwZDpNJmNkVLhBJguHTp1vV4B6C1ttJ25i8ONGbVM7v_0jTRB19Xs9GI_Gl4MxXfoZ-Z3rR2qQbS3QwxqEBo0RCz');
+    }
+    
+      sendNotification(String title, String token)async{
+
+    final data = {
+      'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+      'id': '1',
+      'status': 'done',
+      'message': title,
+    };
+
+    try{
+     http.Response response = await http.post(Uri.parse('https://fcm.googleapis.com/fcm/send'),headers: <String,String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'key=AAAArqJFQfk:APA91bFyFdlX-dk-72NHyaoN0hb4xsp8wuDUhr63ZgI7vroxRSBX1mXbd2pASgdzoYKA_8A0ZYRw61GzRaIH_6eakiVtyr_X8FJrlax-HwJdSUzbk022EGjfVjkDo7dlgYZNXaMfJS4T'
+      },
+      body: jsonEncode(<String,dynamic>{
+        'notification': <String,dynamic> {'title': 'You received new invitiation!','body': 'You received new invitiation!'},
+        'priority': 'high',
+        'data': data,
+        // token of user will receive the notification 
+        'to': '$token'
+      })
+      );
+
+
+     if(response.statusCode == 200){
+       print(" notificatin is sended");
+     }else{
+       print("Error");
+     }
+
+    }catch(e){
+
+    }
+
+  }
+
+    
   Future<void> sendInviation() async {
 
     //calling to create token 
-    storenotificationToken();
+    //storenotificationToken();
 
     try {
       final validate = _formKey.currentState?.validate();
