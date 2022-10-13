d6a027974b7420c9260611d138243cea8fef1c40
Fay Alshalwi <99558064+FayAlshalwi@users.noreply.github.com>
2022-10-13 03:02:15 +0300

    Update appstate.dart

--- taskify/lib/screens/appstate.dart
+++ taskify/lib/screens/appstate.dart
@@ -190,6 +190,22 @@ class AppState extends ChangeNotifier {
 
     notifyListeners();
   }
+   child: Container(
+    margin: const EdgeInsets.fromLTRB(0, 40, 0, 30),
+    height: 600,
+    child: Card(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 440,
+            child: leftColumn,
+          ),
+          mainImage,
+        ],
+      ),
+    ),
+  ),
 
   List<Tasksss> allTasks = [];
   List<DateTime> toHighlight = [];
