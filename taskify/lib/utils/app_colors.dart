a1fd0ed1a3d864a483abd0382d8483e562a4b6c4
RaghadSD <86389094+RaghadSD@users.noreply.github.com>
2022-10-22 18:09:17 +0300

    Sprint 3

---
+++ taskify/lib/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const deepPurple = Colors.deepPurple;
+  static const lightsky = Color(0xFFA6C0FF);
+  static const whiteshade = Color(0xFFF8F9FA);
+
+  static const lightblueshade = Color(0xFF758CC8);
+  static const grayshade = Color(0xFFEBEBEB);
+  static const lightblue = Color(0xFF4B68D1);
+  static const blackshade = Color(0xFF555555);
+  static const hintText = Color(0xFFC7C7CD);
+}
