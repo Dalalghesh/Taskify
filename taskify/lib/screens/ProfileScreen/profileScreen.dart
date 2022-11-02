a3c88ac0fae415b7d17dc5650edf7fe0f1a246b3
Dalalghesh <Dalal.ghesh@gmail.com>
2022-11-03 02:48:22 +0300

    Update profileScreen.dart

--- taskify/lib/screens/ProfileScreen/profileScreen.dart
+++ taskify/lib/screens/ProfileScreen/profileScreen.dart
@@ -630,7 +630,6 @@ class _HomeScreen extends State<HomeScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
                       child: LinearPercentIndicator(
                         width: MediaQuery.of(context).size.width * 0.90,
                         // width: 300.0,
