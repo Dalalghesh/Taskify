ca3b18d887523b9c6f3c535d89a6873ae5fc064d
LeenAbdulaziz1 <leen.567@hotmail.com>
2023-05-27 23:06:16 +0300

    refactor & slicing

--- taskify/lib/invitation/widget/send_invitation_form.dart
+++ taskify/lib/invitation/widget/send_invitation_form.dart
@@ -54,41 +54,36 @@ class _SendInvitationFormState extends State<SendInvitationForm> {
     FirebaseMessaging.instance.subscribeToTopic('subscription');
   }
 
-//Extracted method.
-  Future<void> sendInviation(String recieverEmail, String listId) async {
-    try {
-      // final senderEmail = _firebaseAuth.currentUser?.email;
-      final validate = _formKey.currentState?.validate();
-
-      if (validate ?? false) {
-        _formKey.currentState?.save();
-        // print(email.toString());
-        await context.read<InvitaitonProvider>().sendInvitation(
-            email!, widget.category, widget.list, widget.listId);
-
-        // Provider.of<InvitaitonProvider>(context, listen: false).selectedUser(email);
-        CoolAlert.show(
-          context: context,
-          type: CoolAlertType.success,
-          text: "Invitation sent successfully",
-          confirmBtnColor: const Color(0xff7b39ed),
-          // onConfirmBtnTap: () => route(isChecked),
-        );
-        // showPlatformDialogue(
-        //     context: context, title: "Invitation sent successfully");
-        _formKey.currentState?.reset();
-      }
-    } catch (e) {
-      _formKey.currentState?.reset();
+ Future<void> sendInviation(String recieverEmail, String listId) async {
+  try {
+    final validate = _formKey.currentState?.validate();
+    if (validate ?? false) {
+      _formKey.currentState?.save();
+      await context.read<InvitaitonProvider>().sendInvitation(
+          email!, widget.category, widget.list, widget.listId);
       CoolAlert.show(
         context: context,
-        type: CoolAlertType.error,
-        text: "You can't invite yourself!",
+        type: CoolAlertType.success,
+        text: "Invitation sent successfully",
         confirmBtnColor: const Color(0xff7b39ed),
       );
+      _formKey.currentState?.reset();
     }
-    _typeAheadController.clear();
+  } catch (e) {
+    _formKey.currentState?.reset();
+    CoolAlert.show(
+      context: context,
+      type: CoolAlertType.error,
+      text: "You can't invite yourself!",
+      confirmBtnColor: const Color(0xff7b39ed),
+    );
   }
+  _typeAheadController.clear();
+}
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -140,30 +135,24 @@ class _SendInvitationFormState extends State<SendInvitationForm> {
               final _firebaseFirestore = FirebaseFirestore.instance;
               final senderEmail = _firebaseAuth.currentUser?.email;
 
-             //Rename Variables 
-              final PendingInvitations = await _firebaseFirestore
+              /*final res = await _firebaseFirestore
+                  .collection('invitations')
+                  .where("recieverEmail", isEqualTo: recieverEmail)
+                  .where("senderEmail", isEqualTo: senderEmail)
+                  .where("listId", isEqualTo: widget.listId)
+                  .get();*/
+              final res = await _firebaseFirestore
                   .collection('invitations')
                   .where("recieverEmail", isEqualTo: recieverEmail)
                   .where("senderEmail", isEqualTo: senderEmail)
                   .where("listId", isEqualTo: widget.listId)
                   .where("status", isEqualTo: "pending")
                   .get();
-                
-              //Rename Variables 
-              final AcceptedInvitations = await _firebaseFirestore
-                  .collection('invitations')
-                  .where("recieverEmail", isEqualTo: recieverEmail)
-                  .where("senderEmail", isEqualTo: senderEmail)
-                  .where("listId", isEqualTo: widget.listId)
-                  .where("status", isEqualTo: "accepted")
-                  .get();
 
-
-
-              //Solving Nested if
               //Case1 pending
-              if (PendingInvitations.docs.isNotEmpty) {
-                print("PendingInvitations");
+              if (res.docs.isNotEmpty) {
+                print("helloooooooo");
+                print("dublicate00");
                 CoolAlert.show(
                   context: context,
                   type: CoolAlertType.error,
@@ -171,31 +160,35 @@ class _SendInvitationFormState extends State<SendInvitationForm> {
                   text: "The invitation has already been sent",
                   confirmBtnColor: const Color(0xff7b39ed),
                 );
-                _typeAheadController.clear();}
-              
-              // Solving Nested if
-              //Case2 accepted
-              if (AcceptedInvitations.docs.isNotEmpty) {
-                  print("AcceptedInvitations");
+                _typeAheadController.clear();
+              } else {
+                final res1 = await _firebaseFirestore
+                    .collection('invitations')
+                    .where("recieverEmail", isEqualTo: recieverEmail)
+                    .where("senderEmail", isEqualTo: senderEmail)
+                    .where("listId", isEqualTo: widget.listId)
+                    .where("status", isEqualTo: "accepted")
+                    .get();
+
+                //Case2 accepted
+                if (res1.docs.isNotEmpty) {
+                  print("accepted");
                   CoolAlert.show(
                     context: context,
                     type: CoolAlertType.error,
                     title: "Invitation",
                     text: "Friend already has this list!",
                     confirmBtnColor: const Color(0xff7b39ed),
-                  );}     
-
-                //Solving Nested if
+                  );
+                }
                 //case3+4 Fisrt Time or rejected
-              if (AcceptedInvitations.docs.isEmpty && PendingInvitations.docs.isEmpty){
-                  print("Fisrt Time or rejected");      
-                  //calling Extracted method
+                else {
                   await sendInviation(query, widget.listId);
                   getUsersToken(email.toString());
-                }              
+                }
+              }
               _typeAheadController.clear();
             },
-            
             child: const Text(
               'Invite',
               style: TextStyle(fontSize: 20),
@@ -254,42 +247,53 @@ class _SendInvitationFormState extends State<SendInvitationForm> {
     );
   }
 
-  sendNotification(String title, String token) async {
-    print('dalal');
-    print('raghad');
+ Future<void> sendNotification(String title, String token) async {
+  print('dalal');
+  print('raghad');
 
-    final data = {
-      'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-      'id': '1',
-      'status': 'done',
-      'message': title,
-    };
+  final data = {
+    'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+    'id': '1',
+    'status': 'done',
+    'message': title,
+  };
 
-    try {
-      http.Response response =
-          await http.post(Uri.parse('https://fcm.googleapis.com/fcm/send'),
-              headers: <String, String>{
-                'Content-Type': 'application/json',
-                'Authorization':
-                    'key=AAAArqJFQfk:APA91bFyFdlX-dk-72NHyaoN0hb4xsp8wuDUhr63ZgI7vroxRSBX1mXbd2pASgdzoYKA_8A0ZYRw61GzRaIH_6eakiVtyr_X8FJrlax-HwJdSUzbk022EGjfVjkDo7dlgYZNXaMfJS4T'
-              },
-              body: jsonEncode(<String, dynamic>{
-                'notification': <String, dynamic>{
-                  'title': title,
-                  'body': 'You got invitations from your friend to his list!!'
-                },
-                'priority': 'high',
-                'data': data,
-                'to': '$token'
-              }));
+  final headers = <String, String>{
+    'Content-Type': 'application/json',
+    'Authorization':
+        'key=AAAArqJFQfk:APA91bFyFdlX-dk-72NHyaoN0hb4xsp8wuDUhr63ZgI7vroxRSBX1mXbd2pASgdzoYKA_8A0ZYRw61GzRaIH_6eakiVtyr_X8FJrlax-HwJdSUzbk022EGjfVjkDo7dlgYZNXaMfJS4T'
+  };
 
-      if (response.statusCode == 200) {
-        print("Yeh notificatin is sended");
-      } else {
-        print("Error");
-      }
-    } catch (e) {}
+  final body = jsonEncode(<String, dynamic>{
+    'notification': <String, dynamic>{
+      'title': title,
+      'body': 'You got invitations from your friend to his list!!'
+    },
+    'priority': 'high',
+    'data': data,
+    'to': token,
+  });
+
+  try {
+    final response = await http.post(
+      Uri.parse('https://fcm.googleapis.com/fcm/send'),
+      headers: headers,
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      print("Notification sent successfully");
+    } else {
+      print("Error sending notification");
+    }
+  } catch (e) {
+    print("Error: $e");
   }
+}
+
+
+
+
 
   Future getUsersToken(String receiver) async {
     print('hello');
